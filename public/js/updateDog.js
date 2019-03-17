e48c767224181f26bac2acef30eeeac6f4ffba12
Smushed <smushedcode@gmail.com>
2019-03-17 09:33:14 -0500

    SweetAlert is working

--- public/js/updateDog.js
+++ public/js/updateDog.js
@@ -1,14 +1,15 @@
-$(".deleteDog").on("click", function () {
-    const id = $(".deleteDog").data("id");
-    console.log(id)
-    $.ajax({
-        url: "/api/deletedog",
-        type: "DELETE",
-        data: { id },
-        datatype: "json",
-        success: function (userID) {
-            window.location.replace(`/user/${userID}`)
-        }
-    })
+$(".deleteDog").on("click", () => {
+    Swal.fire('Are you sure you want to delete Doggo?')
+
+    // const id = $(".deleteDog").data("id");
+    // $.ajax({
+    //     url: "/api/deletedog",
+    //     type: "DELETE",
+    //     data: { id },
+    //     datatype: "json",
+    //     success: (userID) => {
+    //         window.location.replace(`/user/${userID}`)
+    //     }
+    // });
 });
 
